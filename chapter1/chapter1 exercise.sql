03ed477166448e25f71827cf1a030423c2ca02d8
Lettie <lettiezulu764@gmail.com>
2023-11-07 00:38:39 +0200

    first commit

---
+++ chapter1/chapter1 exercise.sql
@@ -0,0 +1,4 @@
+"animal_type_id","species","category","average_lifespan","habitat","conservation_status"
+1,"Tiger","Mammal",15,"Jungle","Endangered"
+2,"Elephant","Mammal",60,"Grassland","Vulnerable"
+3,"Penguin","Bird",20,"Cold Regions","Least Concern"

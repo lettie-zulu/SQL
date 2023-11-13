cc34d68850c34ea49ffee09ca18a0dc5a68ccb60
Lettie <lettiezulu764@gmail.com>
2023-11-13 19:00:17 +0200

    changes commited

---
+++ chapter4/chapt4 exercise.sql
@@ -0,0 +1,16 @@
+CREATE TABLE actors (
+    id integer,
+    movie text,
+    actor text
+);
+
+COPY (
+    SELECT geo_name, state_us_abbreviation, housing_unit_count_100_percent
+    FROM us_counties_2010 ORDER BY housing_unit_count_100_percent DESC LIMIT 20
+     )
+TO 'C:\YourDirectory\us_counties_housing_export.txt'
+WITH (FORMAT CSV, HEADER);
+
+-- No, it won't. In fact, you won't even be able to create a column with that
+-- data type because the precision must be larger than the scale. The correct
+-- type for the example data is numeric(8,3).
